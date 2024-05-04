4f972d31af7faa0a85bac4e4b33a864f504939bd
BenHl <benhlance@gmail.com>
2024-05-04 00:24:45 -0400

    Initial openGL environment configuration
    Compile using: g++ test.cpp -o test -lGL -lGLEW -lglfw
     or:  ./run
    glew and glfw are downloaded locally
    run in wsl

---
+++ build/CMakeFiles/GoL2d.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for GoL2d.
+# This may be replaced when dependencies are built.
---
+++ test.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <GL/glew.h>
+#include <GL/gl.h>
+#include <GLFW/glfw3.h>
+
+
+using namespace std;
+
+int main() {
+
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW" << std::endl;
+        return -1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // Create a windowed mode window and its OpenGL context
+    GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL Window", NULL, NULL);
+    if (!window) {
+        std::cerr << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    // Make the window's context current
+    glfwMakeContextCurrent(window);
+
+    // Initialize GLEW
+    if (glewInit() != GLEW_OK) {
+        std::cerr << "Failed to initialize GLEW" << std::endl;
+        return -1;
+    }
+
+    // Enable depth testing
+    glEnable(GL_DEPTH_TEST);
+
+    // Rendering loop
+    while (!glfwWindowShouldClose(window)) {
+        // Clear the color and depth buffers
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        // Render here
+
+
+        // Swap front and back buffers
+        glfwSwapBuffers(window);
+
+        // Poll for and process events
+        glfwPollEvents();
+    }
+
+    // Terminate GLFW
+    glfwTerminate();
+
+
+    std::cout << "Hello World!\n";
+    return 0;
+}
